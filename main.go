d4053c0f64a4dcc6122da3e342b89f4aa2a4009e
Sam Vann <sam.vann42:gmail.com>
2024-04-29 14:41:16 +0100

    sec checks

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import (
 
 	"github.com/bootdotdev/learn-cicd-starter/internal/database"
 
-	_ "tursodatabase/libsql-client-go/libsql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type apiConfig struct {
