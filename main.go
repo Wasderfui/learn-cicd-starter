3f1f835fdd7644e412732c2f06baaf58bf56f4de
Sam Vann <sam.vann42:gmail.com>
2024-05-01 23:11:09 +0100

    ???

--- main.go
+++ main.go
@@ -13,11 +13,11 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
-	"github.com/joho/godotenv"
+	//"github.com/joho/godotenv"
 
 	"github.com/bootdotdev/learn-cicd-starter/internal/database"
 
-	_ "github.com/go-sql-driver/mysql"
+	 "github.com/go-sql-driver/mysql"
 )
 
 type apiConfig struct {
